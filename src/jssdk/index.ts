85252e03cf1f8d940a5dbd153d8d404f32bffaad
Oliver.W <ole3021@gmail.com>
2018-07-04 17:40:55 +0800

    add env config

--- src/jssdk/index.ts
+++ src/jssdk/index.ts
@@ -1,21 +1,11 @@
 import { Module } from '@nestjs/common';
 import { JSSDKController } from './controller';
 import { JSSDKService } from './service';
-import { WeChatModule } from '../wechat';
+import { ConfigModule } from '../config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule],
   controllers: [JSSDKController],
-  providers: [
-    JSSDKService,
-    {
-      provide: 'WECHAT_APP_ID',
-      useValue: 'wx186c10fc6a4ff63c',
-    },
-    {
-      provide: 'WECHAT_SECRET',
-      useValue: '741deef4b669efa309c76aebf2b0c9ac',
-    },
-  ],
+  providers: [JSSDKService],
 })
 export class JSSDKModule {}
